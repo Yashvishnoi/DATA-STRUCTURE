dc3d4c0ec600ea31cb047fbae47f5962ed250215
Yashvishnoi <jivishnoi@gmail.com>
2020-03-27 01:24:15 +0530

    Done iteration of program

--- LL_InsertingANodeAtBegning.cpp
+++ LL_InsertingANodeAtBegning.cpp
@@ -3,42 +3,42 @@ using namespace std;
 
 struct Node
 {
-int data;
-Node* next;
+	int data;
+	Node* next;
 };
-Node* head;
-void Insert(int x)
+	Node* head;
+	void Insert(int x)
 {
-Node* temp=new Node;
-temp->data=x;
-temp->next=head;
-head=temp;
+	Node* temp=new Node;
+	temp->data=x;
+	temp->next=head;
+	head=temp;
 }
 
 void Print()
 {
-Node* temp = head;
-while(temp!=NULL)
-{
-cout<<" "<< temp->data;
-temp=temp->next;
-}
-cout<<" ";
+	Node* temp = head;
+	while(temp!=NULL)
+	{
+		cout<<" "<< temp->data;
+		temp=temp->next;
+	}
+	cout<<" ";
 }
 
 
 
 int main()
 {
-head=NULL;
-int n,x,i;
-cin>>n;
-for(i=0;i<n;i++)
-{
-cout<<" Enter the number to be entered ";
-cin>>x;
-Insert(x);
-Print();
-}
+	head=NULL;
+	int n,x,i;
+	cin>>n;
+	for(i=0;i<n;i++)
+	{
+		cout<<" Enter the number to be entered ";
+		cin>>x;
+		Insert(x);
+		Print();
+	}
 return 0;
 }
